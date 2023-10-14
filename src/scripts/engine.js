b0e76e7f3192bc7f4624ff8d188f9f9aca01766a
Bruno de Souza Farias <108137122+BrunoSouzaFarias@users.noreply.github.com>
2023-10-13 22:29:22 -0300

    Add decreaseLife, playsong, countDown

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,7 +3,8 @@ const state = {
         squares: document.querySelectorAll('.square'),
         enemy: document.querySelector('.enemy'),
         timeLeft: document.querySelector('#time-left'),
-        score: document.querySelector('#score')
+        score: document.querySelector('#score'),
+        livesLeft: document.querySelector('#lives-left')
     },
     Values:{
         timerId: null,
@@ -11,8 +12,30 @@ const state = {
         hitPosition: 0,
         result: 0,
         currentTime: 60,
+        lives: 3,
+       
+    },
+    actions: {
+        countDownTimerId:setInterval(countDown, 1000),
+        
     },
 };
+
+function countDown (){
+    state.Values.currentTime--;
+    state.view.timeLeft.textContent = state.Values.currentTime;
+    if(state.Values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.currentTime.timerId);
+        alert("Game Over!! O seu Resultado foi:" + state.Values.result);
+    }
+}
+
+function playSond(){
+    let audio = new Audio('./src/audioshit');
+    audio.volume = 0.2;
+    audio.play();
+}
 
 function randomSquare () {
     state.view.squares.forEach((square) => {
@@ -31,17 +54,28 @@ function moveEnemy (){
 
 function addListenerHitbox() {
     state.view.squares.forEach((square) => {
-       square.addEventListener('mousedown', () => {
-        if (square.id === state.Values.hitPosition) {
-            state.Values.result++
-            state.view.score.textContent = state.Values.result;
-            state.Values.hitPosition = null;
-        }
-       } )
-    })
+        square.addEventListener('mousedown', () => {
+            if (square.id === state.Values.hitPosition) {
+                state.Values.result++;
+                state.view.score.textContent = state.Values.result;
+                state.Values.hitPosition = null;
+                playSound();
+            } else {
+               decreaseLife();
+            }
+        });
+    });
 }
 
+function decreaseLife(){
+    state.Values.lives --;
+    state.view.livesLeft.textContent = state.Values.lives;
 
+    if(state.Values.lives <= 0) {
+        clearInterval(state.Values.timerId);
+        alert("Gamer Over!!! Você perderu todas suas vidas o seu Resultado Foi: " +state.Values.result);
+    }
+}
 
 
 
